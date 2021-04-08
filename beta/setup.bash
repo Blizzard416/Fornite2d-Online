889f892b33641d7296c0fc16a1480ea8c6d58543
congyoua <77039291+congyoua@users.noreply.github.com>
2021-04-08 14:19:53 -0400

    Add files via upload

---
+++ beta/setup.bash
@@ -0,0 +1,17 @@
+npm install express
+
+npm install react-router-dom
+
+npm install @material-ui/core
+
+npm install mongoose
+
+npm install validator
+
+npm install bcryptjs
+
+npm install axios
+
+npm install react-scripts
+
+npm install
